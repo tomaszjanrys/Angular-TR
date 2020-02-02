dffa6d09e65e805ad22be214945934d4655add09
tomaszjanrys <tomaszjanrys@gmail.com>
2020-02-02 21:44:38 +0100

    Wykres gotowy

--- src/app/wykres/wykres.component.ts
+++ src/app/wykres/wykres.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wykres.component.less']
 })
 export class WykresComponent implements OnInit {
+  public baroptions = {
+    scaleShowVerticalLIne: false,
+    responsive: true};
+    public barLabes = ['a', '3'];
+    public barlegend = true ;
+    public barType = 'bar';
+    public barchart = [
+      {data: [3, 4] , label: 'a'},
+      {data: [4, 42] , label: 'a'}
+    ];
+
+
 
   constructor() { }
 
