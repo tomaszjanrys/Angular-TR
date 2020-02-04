2c6732a8322768084693fec6c48ef30c331b921e
tomaszjanrys <tomaszjanrys@gmail.com>
2020-02-04 16:05:00 +0100

    +

--- src/app/dom/dom.component.ts
+++ src/app/dom/dom.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-dom',
@@ -6,8 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dom.component.less']
 })
 export class DomComponent implements OnInit {
-
   constructor() { }
+  imie = '';
+  liczbax = 11;
+  data = [{
+    name : 'tomek',
+    nazwisko : 'Rys',
+  }, {
+    name: 'Natalia',
+    nazwisko: 'Ryś',
+  }, {
+    name: 'Hania',
+    nazwisko: 'Zdebel'
+  },{
+    name: 'Donald',
+    nazwisko: 'Trup'
+  },{
+    name: 'Jacek',
+    nazwisko: 'Duda'
+  }];
+  zmiana = false;
+  zmiana2 = false;
+
+
+
+liczenie = () => {
+  this.liczbax = this.liczbax + 1;
+  if (this.liczbax > 10) {
+this.zmiana = true;
+  } else {
+    this.zmiana = false;
+  }
+
+}
+liczeniex = () => {
+  this.liczbax = this.liczbax - 1;
+  if (this.liczbax < 0) {
+    this.zmiana2 = true;
+      } else {
+        this.zmiana2 = false;
+      }
+
+}
+
 
   ngOnInit() {
   }
--- src/app/wykres/wykres.component.ts
+++ src/app/wykres/wykres.component.ts
@@ -8,7 +8,7 @@ import { Chart } from 'chart.js';
 })
 export class WykresComponent implements OnInit {
 
-czart = [];
+czart: any[] = [];
 czartdwa = [];
   public baroptions = {
     scaleShowVerticalLIne: true,
@@ -17,7 +17,7 @@ czartdwa = [];
   };
     public barLabes = ['Day 1', 'Day 2' ,'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7', 'Day 8' , "3 luty 2020", 'Day 10', 'Day 11', 'Day 12'];
     public barlegend = true ;
-    public barType = 'bar';
+    public barType = 'line';
     public barchart = [
       {data: [1900,2800,4600, 6000, 7750, 9745, 11820,14380, 17205  ] , label: 'Zarazenia potwierdzone'},
       {data: [56, 80, 106, 130, 170, 213, 259, 0 ,361] , label: 'Deaths'},
@@ -46,14 +46,54 @@ czartdwa = [];
   ngOnInit() {
     this.czart[''] = new Chart('canvas', {
       type: 'line',
+
       data: {labels: ['Ocena 6', 'Ocena 5', 'Ocena 4', 'Ocena 3', 'Ocena 2', 'Ocena 1'],
       datasets:[{
-        label:'kasa',
+        label: 'Ilosc ocen',
         data:[13,18,14,13,3,2],
         lineTension: 0.4 ,
-        backgroundColor:['rgb(146, 199, 0, 1)'],
+        backgroundColor:['rgb(146, 199, 0, 0.5)'],
         borderColor:'rgb(8, 31, 245)',
-        borderWidth:4
+        borderWidth:1,
+        radius:10,
+        pointStyle:'circle',
+        pointBorderColor:'black',
+        fill: true
+
+
+      },{
+        label: 'Matematyka ilosc ocen',
+        data:[0,2,2,1,0,0],
+        lineTension: 0.4 ,
+        backgroundColor:['rgb(123, 225, 238, 0.5)'],
+        borderColor:'rgb(248, 80, 2)',
+        borderWidth:2,
+        radius:10,
+        pointStyle:'cross',
+        pointBorderColor:'black',
+        fill: true
+      },{
+        label: 'Polski ilosc ocen',
+        data:[1,3,3,4,2,0],
+        lineTension: 0.4 ,
+        backgroundColor:['rgb(123, 225, 238, 0.5)'],
+        borderColor:'rgb(248, 2, 248)',
+        borderWidth:2,
+        radius:10,
+        pointStyle:'star',
+        pointBorderColor:'black',
+        fill: true
+      },{
+        label: 'Geografia ilosc ocen',
+        data:[0,2,2,1,2,0],
+        lineTension: 0.4 ,
+        backgroundColor:['rgb(123, 225, 238, 0.5)'],
+        borderColor:'rgb(243, 239, 243)',
+        borderWidth:2,
+        radius:10,
+        pointStyle:'star',
+        pointBorderColor:'black',
+        fill: true
       }]
 
     },
@@ -61,12 +101,18 @@ czartdwa = [];
       title:{
         text:"Ilosc ocen Natalia ",
         display:true,
-        fontColor: 'red'
+        fontColor: 'red',
+        fontSize: 20,
+        fontStyle: 'italic',
+        position: "right",
+
+
       },
       scales:{
         yAxes:[{
           ticks:{
-            beginAtZero:false
+            beginAtZero:false,
+
           }
         }]
       }
